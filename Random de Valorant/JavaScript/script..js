709a7e2c67292e96275337345c1aabab4adf2177
Gabriel Cunha <106206308+Gabriel-Estevao@users.noreply.github.com>
2025-03-26 15:23:50 -0300

    Add files via upload

--- Random de Valorant/JavaScript/script..js
+++ Random de Valorant/JavaScript/script..js
@@ -1,30 +1,52 @@
 const imagens = [
-    "imagem1-1.jpg",
-    "imagem1-2.jpg",
-    "imagem1-3.jpg",
-    "imagem2-1.jpg",
-    "imagem2-2.jpg",
+  "imagens/Astra.webp",
+  "imagens/Breach.webp",
+  "imagens/Brimstone.webp",
+  "imagens/Chamber.webp",
+  "imagens/Clove.webp",
+  "imagens/Cypher.webp",
+  "imagens/Deadlock.webp",
+  "imagens/Fade.webp",
+  "imagens/Gekko.webp",
+  "imagens/Harbor.webp",
+  "imagens/iso.webp",
+  "imagens/Jett.webp",
+  "imagens/KAYO.webp",
+  "imagens/Killjoy.webp",
+  "imagens/Neon.webp",
+  "imagens/Omen.webp",
+  "imagens/Phoenix.webp",
+  "imagens/Raze.webp",
+  "imagens/Reyna.webp",
+  "imagens/Sage.webp",
+  "imagens/Skye.webp",
+  "imagens/Sova.webp",
+  "imagens/Tejo.webp",
+  "imagens/Viper.webp",
+  "imagens/VYSE.webp",
+  "imagens/Waylay.webp",
+  "imagens/Yoru.webp",
     // ... mais imagens com o mesmo padrão de nomeação
   ];
 
-  function filtrarImagens(nomeImagem) {
-    return imagens.filter(imagem => imagem.includes(nomeImagem));
+  const elementosImagem = document.querySelectorAll(".imagem-galeria");
+  const botaoGerar = document.getElementById("botaoGerar");
+  let imagensExibidas = [];
+  
+  function gerarImagensAleatorias() {
+      imagensExibidas = []; // Limpa as imagens exibidas anteriormente
+      for (let i = 0; i < elementosImagem.length; i++) {
+          let indiceAleatorio;
+          do {
+              indiceAleatorio = Math.floor(Math.random() * imagens.length);
+          } while (imagensExibidas.includes(imagens[indiceAleatorio])); // Garante que a imagem não se repita
+  
+          imagensExibidas.push(imagens[indiceAleatorio]);
+          elementosImagem[i].src = imagens[indiceAleatorio];
+      }
   }
-
-  function gerarImagemAleatoria() {
-    const imagensFiltradas = filtrarImagens(imagemAtualNome);
-    if (imagensFiltradas.length > 0) {
-      const indiceAleatorio = Math.floor(Math.random() * imagensFiltradas.length);
-      return imagensFiltradas[indiceAleatorio];
-    } else {
-      // Nenhuma imagem com o mesmo nome encontrada, retorne uma imagem aleatória do array completo
-      return imagens[Math.floor(Math.random() * imagens.length)];
-    }
-  }
-
-  const imagemElement = document.getElementById("minhaImagem");
-
-  setInterval(() => {
-    const imagemAleatoriaURL = gerarImagemAleatoria();
-    imagemElement.src = imagemAleatoriaURL;
-  }, 5000);
+  
+  botaoGerar.addEventListener("click", gerarImagensAleatorias);
+  
+  // Gera as primeiras 5 imagens aleatórias ao carregar a página
+  gerarImagensAleatorias();
